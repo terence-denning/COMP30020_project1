4fcafd6da5d281a24c6593a1a52a41e57a988bfa
Terence Denning <terrencedenning@gmail.com>
2019-08-24 15:23:12 +1000

    remove card added

--- Proj1.hs
+++ Proj1.hs
@@ -2,8 +2,17 @@
 
 import Card
 
---feedback :: [Card] → [Card] → (Int,Int,Int,Int,Int)
-
+feedback :: [Card] -> [Card] -> (Int,Int,Int,Int,Int)
+feedback [] [] = (0,0,0,0,0)
+feedback x [] = (0,0,0,0,0)
+feedback [] x = (0,0,0,0,0)
+feedback guesses answers = ( 
+    numCorrectCards guesses answers,
+    cardsWithLowerRank guesses answers,
+    cardsWithSameRank guesses answers,
+    cardsWithHigherRank guesses answers,
+    cardsWithSameSuit guesses answers 
+    )
 
 
 --initialGuess :: Int → ([Card],GameState)
@@ -21,15 +30,15 @@ cardsWithSameRank :: [Card] -> [Card] -> Int
 cardsWithSameRank [] [] = 0
 cardsWithSameRank x [] = 0
 cardsWithSameRank [] x = 0
-cardsWithSameRank guesses answers = length [ rank | (Card _ rank) <- guesses, rank `elem` [ rank | (Card _ rank) <- answers ] ] 
+cardsWithSameRank guesses answers = length [ rank | (Card _ rank) <- answers, rank `elem` [ rank | (Card _ rank) <- guesses ] ] 
 
 -- returns number of cards from answer with lower rank than lowest ranking card in guesses
 -- takes in guesses as first parameter and answers as second parameter
-cardsWithLowestRank :: [Card] -> [Card] -> Int
-cardsWithLowestRank _ [] = 0
-cardsWithLowestRank guesses ((Card _ rank) : answers)
-    | rank < lowestRank(guesses) = 1 + cardsWithLowestRank guesses answers 
-    | otherwise = cardsWithLowestRank guesses answers
+cardsWithLowerRank :: [Card] -> [Card] -> Int
+cardsWithLowerRank _ [] = 0
+cardsWithLowerRank guesses ((Card _ rank) : answers)
+    | rank < lowestRank(guesses) = 1 + cardsWithLowerRank guesses answers 
+    | otherwise = cardsWithLowerRank guesses answers
 
 -- return lowest rank from list of cards
 lowestRank :: [Card] -> Rank
@@ -37,3 +46,34 @@ lowestRank [(Card _ rank)] = rank
 lowestRank ( ( Card xsuit xrank ) : ( ( Card ysuit yrank ) : ys ) )
     | yrank < xrank = lowestRank ( ( Card ysuit yrank ) : ys )
     | otherwise = lowestRank ( ( Card xsuit xrank ) : ys )
+
+
+-- returns number of cards from answer with higher rank than highest ranking card in guesses
+-- takes in guesses as first parameter and answers as second parameter
+cardsWithHigherRank :: [Card] -> [Card] -> Int
+cardsWithHigherRank _ [] = 0
+cardsWithHigherRank guesses ((Card _ rank) : answers)
+    | rank > highestRank(guesses) = 1 + cardsWithHigherRank guesses answers 
+    | otherwise = cardsWithHigherRank guesses answers
+
+-- return highest rank from list of cards
+highestRank :: [Card] -> Rank
+highestRank [(Card _ rank)] = rank
+highestRank ( ( Card xsuit xrank ) : ( ( Card ysuit yrank ) : ys ) )
+    | yrank > xrank = highestRank ( ( Card ysuit yrank ) : ys )
+    | otherwise = highestRank ( ( Card xsuit xrank ) : ys )
+
+
+-- number of cards with same suit
+cardsWithSameSuit :: [Card] -> [Card] -> Int
+cardsWithSameSuit [] [] = 0
+cardsWithSameSuit x [] = 0
+cardsWithSameSuit [] x = 0
+cardsWithSameSuit guesses answers = length [ suit | (Card suit _) <- answers, suit `elem` [ suit | (Card suit _) <- guesses ] ] 
+
+-- remove a card from a list of cards
+removeCard :: Card -> [Card] -> [Card]
+removeCard _ [] = []
+removeCard ( Card suit rank ) ( ( Card suit' rank' ) : cards )
+    | suit == suit' && rank == rank' = removeCard ( Card suit rank ) cards
+    | otherwise = ( Card suit' rank' ) : removeCard ( Card suit rank ) cards
